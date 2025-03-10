cc6670155f85e520519e298ce23e28f861b2d2b9
AzizMardonqulov <=>
2025-03-10 14:41:52 +0500

    `Refactor CSS: updated styles and layout properties` and fix websayt

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let bars = document.getElementById("bars")
 let NavbarItems = document.getElementById("NavbarItems")
-var num =1;
+let navbar = document.getElementById("navbar")
 bars.addEventListener('click' , function(){
     bars.classList.toggle('res');
     NavbarItems.classList.toggle("item")
+})
+window.addEventListener('scroll' , function(){
+    if(window.scrollY > 0){
+        navbar.classList.add("navbarSkroll");
+    }else{
+        navbar.classList.remove("navbarSkroll")
+    }
 })
